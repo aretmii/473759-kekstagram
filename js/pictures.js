a6cf5abd398a37e5261ef59c324f5404250c0686
unknown <geolog197@mail.ru>
2018-02-07 22:56:18 +0300

    Second

--- js/pictures.js
+++ js/pictures.js
@@ -2,32 +2,31 @@
 
 var PHOTOS = [];
 var COMMENTS = ['Всё отлично!, В целом всё неплохо. Но не всё.', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.', 'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
-var photoObj = {};
-//записываем в переменную DOM элемент
-  var similarPictureElement = document.querySelector('.pictures');
+// записываем в переменную DOM элемент
+var similarPictureElement = document.querySelector('.pictures');
 var pictureTemplate = document.querySelector('#picture-template').content;
-//функция генерации случайных данных
+// функция генерации случайных данных
 var randomNum = function getRandomNum(min, max) {
   var i = Math.floor(Math.random() * (max - min)) + min;
   return i;
-}
-//функция создания массива из 25 генерируемых объектов
-var renderPhoto = function (photo) {
-  //создаем объект
+};
+// функция создания массива из 25 генерируемых объектов
+var renderPhoto = function (^photo) {
+  // создаем объект
   var photoObj = {};
-  //выбираем рандомный номер фотографии
+  // выбираем рандомный номер фотографии
   var i = randomNum(1, 25);
-  //подставляем рандомный адрес картинки в url объекта
+  // подставляем рандомный адрес картинки в url объекта
   photoObj.url = 'photos/' + i + '.jpg';
-  //выбираем рандомное кол-во лайков
+  // выбираем рандомное кол-во лайков
   var likes = randomNum(15, 200);
-  //подставляем рандомное кол-во лайков в likes объекта
+  // подставляем рандомное кол-во лайков в likes объекта
   photoObj.likes = likes;
-  //выбираем рандомный комментарий
-  var comment = randomNum(1,6);
-  //подставляем комментарий в объект
+  // выбираем рандомный комментарий
+  var comment = randomNum(1, 6);
+  // подставляем комментарий в объект
   photoObj.comments = COMMENTS[comment];
-  //записываем объект в массив
+  // записываем объект в массив
   PHOTOS[j] = photoObj;
 
   var photoElement = pictureTemplate.cloneNode(true);
@@ -37,8 +36,8 @@ var renderPhoto = function (photo) {
   photoElement.querySelector('.picture-comments').textContent = PHOTOS[j].comments;
 
   return photoElement;
-}
-//функция записи DocumentFragment
+};
+// функция записи DocumentFragment
 var fragment = document.createDocumentFragment();
 for (var j = 0; j < 26; j++) {
   fragment.appendChild(renderPhoto(PHOTOS[j]));
@@ -47,5 +46,5 @@ for (var j = 0; j < 26; j++) {
   document.querySelector('.comments-count').textContent = PHOTOS[j].comments;
 }
 similarPictureElement.appendChild(fragment);
-//убираем класс hidden из popup
+// убираем класс hidden из popup
 document.querySelector('.gallery-overlay').classList.remove('hidden');
